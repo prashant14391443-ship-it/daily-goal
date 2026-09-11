12bea82b184895afdb869b4500116a9be8208757
daily-goal <prashant14391443@gmail.com>
2026-09-11 12:21:24 +0530

    fix empty pyq page

---
+++ app/pyq/page.tsx
@@ -0,0 +1,8 @@
+
+export default function PYQPage() {
+  return (
+    <div className="min-h-screen bg-slate-950 text-white flex items-center justify-center">
+      <h1 className="text-2xl font-bold">Previous Year Questions Coming Soon!</h1>
+    </div>
+  );
+}
